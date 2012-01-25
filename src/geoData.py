40edff9bbf5d0d357041d9a3db8e9f62f67ac49c
Stefan Haertel <haertel.dev@gmail.com>
2012-01-25 16:33:28 +0100

    we now have objects!

--- src/geoData.py
+++ src/geoData.py
@@ -1,20 +1,43 @@
-class Region:
-	def __init__(self, obj):
-		self.geometry = obj["geometry"]
-		self.type = obj["type"]
-		self.properties = obj["properties"]
-		print(self.properties)
+class Polygon:
+	def __init__(self, geometry):
+		self.coordinates = geometry[0]
 		pass
 
+class PolygonMulti:
+	def __init__(self, geometry):
+		pass
+
+class Region:
+	def __init__(self, obj):
+		properties = obj["properties"]
+		geometry = obj["geometry"]
+		
+		self.name = properties["PLZORT99"]
+		self.number = properties["PLZ99"]
+		self.geometry = self.getGeometry(geometry)
+	
+	def getGeometry(self, geometry):
+		if geometry["type"] == "Polygon":
+			return Polygon(geometry["coordinates"])
+		else:
+			return PolygonMulti(geometry["coordinates"])
+	
+	def getGeometryType(self):
+		return type(self.geometry).__name__
+	
+	def printData(self):
+		print("Name: " + self.name)
+		print("Number: " + self.name)
+		print("GeometryType: " + self.getGeometryType())
 
 class GeoData:
 	def __init__(self, obj):
-		self.type = obj["type"]
-		self.features = obj["features"]
+		data = obj["features"]
 		self.regions = []
-		for item in self.features:
+		for item in data:
 			self.regions.append(Region(item))
 	
 	def printData(self):
-		#print(self.features)
+		for item in self.regions:
+			item.printData()
 		pass
