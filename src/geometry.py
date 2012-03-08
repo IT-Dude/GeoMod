c78b62cf3d32e5f4c277d61b9eef1d89e9001f50
Stefan Haertel <haertel.dev@gmail.com>
2012-03-08 15:33:38 +0100

    some little illustration

--- src/geometry.py
+++ src/geometry.py
@@ -3,6 +3,18 @@ class Point:
 		self.x = newX
 		self.y = newY
 
+#                  N3
+#	  Vert1 X+++++++++++++++X Vert2
+#			 +             +
+#			  +           +
+#			   +         +
+#			N2  +       +  N1
+#			     +     +
+#			      +   +
+#				   + +
+#					X
+#                 Vert3
+
 class Triangle:
 	def __init__(self):
 		self.vertices = []
